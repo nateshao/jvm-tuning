e6d891e58778ea4479b88e742de7298c953cf0fd
nateshao <shaotongjie@gmail.com>
2020-08-04 22:49:01 +0800

    项目初始化完成

--- java-tuning-test/src/com/nateshao/Main.java
+++ java-tuning-test/src/com/nateshao/Main.java
@@ -1,8 +1,31 @@
 package com.nateshao;
 
+import java.io.IOException;
+
+/**
+ * @date Created by 邵桐杰 on 2020/8/4 22:38
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ *
+ * jps
+ * jps -q
+ * jps -m
+ * jps -l
+ * jps -v
+ * jps -V
+ *      可以组合使用 ： jps -lv
+ *
+ *
+ *
+ */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
+        System.out.println("jps");
+        System.in.read();
     }
 }
---
+++ java-tuning-test/src/com/nateshao/test/JpsDemp01.java
@@ -0,0 +1,12 @@
+package com.nateshao.test;
+
+/**
+ * @date Created by 邵桐杰 on 2020/8/4 22:48
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
+public class JpsDemp01 {
+}
