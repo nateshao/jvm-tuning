968631f3ee05b03744ecba642a9630a7304a8377
nateshao <shaotongjie@gmail.com>
2020-08-04 23:11:22 +0800

    项目初始化完成

--- java-tuning-test/src/com/nateshao/test/JstatDemo02.java
+++ java-tuning-test/src/com/nateshao/test/JstatDemo02.java
@@ -3,29 +3,19 @@ package com.nateshao.test;
 import java.io.IOException;
 
 /**
- * @date Created by 邵桐杰 on 2020/8/4 22:38
+ * @date Created by 邵桐杰 on 2020/8/4 23:10
  * @微信公众号 千羽的编程时光
  * @个人网站 www.nateshao.cn
  * @博客 https://nateshao.gitee.io
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  *
- * jps
- * jps -q
- * jps -m
- * jps -l
- * jps -v
- * jps -V
- *      可以组合使用 ： jps -lv
- *
- *
- *
+ * jstat -gc 5080 250 10
  */
-public class JpsDemp01 {
+public class JstatDemo02 {
     public static void main(String[] args) throws IOException {
         // write your code here
         System.out.println("jstat");
         System.in.read();
     }
 }
-
